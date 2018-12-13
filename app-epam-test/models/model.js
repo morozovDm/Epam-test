03e451558ebb52454e4f1af5ea2a0495d681dccf
morozovDm <44220292+morozovDm@users.noreply.github.com>
2018-12-13 17:04:56 +0400

    Add files via upload

---
+++ app-epam-test/models/model.js
@@ -0,0 +1,133 @@
+model = [  
+    {
+        "date": 1544702100000,
+        "temperature": {
+            "night": -3,
+            "day": 2
+        },
+        "cloudiness": "Ясно",
+        "snow": false,
+        "rain": false
+    },
+    {
+        "date": 1544616180000,
+        "temperature": {
+            "night": 0,
+            "day": 4
+        },
+        "cloudiness": "Облачно",
+        "snow": false,
+        "rain": true
+    },
+    {
+        "date": 1544875380000,
+        "temperature": {
+            "night": 0,
+            "day": 1
+        },
+        "cloudiness": "Облачно",
+        "snow": true,
+        "rain": true
+    },
+    {
+        "date": 1544702100000,
+        "temperature": {
+            "night": -3,
+            "day": 2
+        },
+        "cloudiness": "Ясно",
+        "snow": false,
+        "rain": false
+    },
+    {
+        "date": 1544616180000,
+        "temperature": {
+            "night": 0,
+            "day": 4
+        },
+        "cloudiness": "Облачно",
+        "snow": false,
+        "rain": true
+    },
+    {
+        "date": 1544875380000,
+        "temperature": {
+            "night": 0,
+            "day": 1
+        },
+        "cloudiness": "Облачно",
+        "snow": true,
+        "rain": true
+    },
+    {
+        "date": 1544702100000,
+        "temperature": {
+            "night": -3,
+            "day": 2
+        },
+        "cloudiness": "Ясно",
+        "snow": false,
+        "rain": false
+    },
+    {
+        "date": 1544616180000,
+        "temperature": {
+            "night": 0,
+            "day": 4
+        },
+        "cloudiness": "Облачно",
+        "snow": false,
+        "rain": true
+    },
+    {
+        "date": 1544875380000,
+        "temperature": {
+            "night": 0,
+            "day": 1
+        },
+        "cloudiness": "Облачно",
+        "snow": true,
+        "rain": true
+    },
+    {
+        "date": 1544702100000,
+        "temperature": {
+            "night": -3,
+            "day": 2
+        },
+        "cloudiness": "Ясно",
+        "snow": false,
+        "rain": false
+    },
+    {
+        "date": 1544616180000,
+        "temperature": {
+            "night": 0,
+            "day": 4
+        },
+        "cloudiness": "Облачно",
+        "snow": false,
+        "rain": true
+    },
+    {
+        "date": 1544875380000,
+        "temperature": {
+            "night": 0,
+            "day": 1
+        },
+        "cloudiness": "Облачно",
+        "snow": true,
+        "rain": true
+    },
+    {
+        "date": 1544788980000,
+        "temperature": {
+            "night": 0,
+            "day": 1
+        },
+        "cloudiness": "Облачно",
+        "snow": true,
+        "rain": true
+    }
+    
+]
---
+++ app-epam-test/scripts/main.js
@@ -0,0 +1,73 @@
+const weekday = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+const month = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
+
+function createDayComponent(day) {
+    const today = new Date();
+    let date = new Date(day.date);
+    let li = '<li class="day">';
+    if (date.toLocaleDateString() === today.toLocaleDateString()) {
+        li += `<div>Сегодня</div>`
+    }
+    else {
+        li += `<div>${weekday[date.getDay()]}</div>`;
+    }
+    li += `<h2>${date.getDate()} ${month[date.getMonth()]}</h2>`;
+    li += `<img alt='weather' src=${`./resources/svg/${day.cloudiness}.svg`}></img>`;
+    li += `<h2>днем ${day.temperature.day > 0 ? `+${day.temperature.day}` : day.temperature.day} &#176</h2>`;
+    li += `<div class ='lowerText'>ночью ${day.temperature.night > 0 ? `+${day.temperature.night}` : day.temperature.night} &#176</div>`;
+    li += `<div class ='lowerText'>${day.cloudiness} </div>`
+    li += '</li>';
+    return li;
+}
+
+function createWeatherViewerComponent() {
+    let ul = document.getElementById('weatherView');
+    for (let i = 0; i < model.length; i++) {
+        let li = createDayComponent(model[i]);
+        if (li !== undefined)
+            ul.innerHTML += li;
+    }
+}
+
+function sortModel() {
+    model.sort((a, b) => {
+        if (a.date > b.date) return 1;
+        if (a.date < b.date) return -1;
+    })
+}
+
+
+function prev() {
+    var carousel = document.querySelector('ul');
+    let prevMargin = parseInt(window.getComputedStyle(carousel).marginLeft, 10);
+    if (prevMargin < -177)
+        carousel.style.marginLeft = (prevMargin + 177) + 'px'
+    else
+        carousel.style.marginLeft = '0px'
+}
+
+function next() {
+    var carousel = document.querySelector('ul');
+    let prevMargin = parseInt(window.getComputedStyle(carousel).marginLeft, 10);
+    let list = document.getElementsByClassName('day');
+    if (prevMargin > -177 * (list.length - 4))
+        carousel.style.marginLeft = (prevMargin - 177) + 'px';
+    else
+        carousel.style.marginLeft = -177 * (list.length - 4) + 'px';
+}
+
+function showTodayWeather() {
+    let daysList = document.getElementsByClassName('day');
+    for (let day of daysList) {
+        if (day.firstChild.innerHTML !== "Сегодня") {
+            next()
+        }
+        else return;
+    }
+}
+
+window.onload = () => {
+    sortModel();
+    createWeatherViewerComponent();
+    showTodayWeather();
+}
